48ffcc5a1b74784bfc475d26fad9d6e4c708541c
quinleys <quinleys@student.arteveldehs.be>
2017-10-05 11:31:14 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+    <?php
+    $x = 10;
+    $y = 7;  
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Oefening 3</title>
+</head>
+<body>
+    <p> <?php echo $x - $y ?> </p>
+    <p> <?php echo $x + $y ?> </p>
+    <p> <?php echo $x * $y ?> </p>
+    <p> <?php echo $x / $y ?> </p>
+    <p> <?php echo $x % $y ?> </p>
+</body>
+</html>
